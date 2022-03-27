0f75b9fcb0bde6eb82c60230e9b0a93ccbc55f81
francivaldo4334 <frncvldreis@gmail.com>
2022-03-27 00:49:31 -0300

    emil sent 5

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,24 +1,24 @@
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth:{
-        user: "frncvldreisdev@gmail.com",
-        pass: "fr@nssddev"
-    },
-    tls:{
-        rejectUnauthorized: false,
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'frncvldreisdev@gmail.com',
+      pass: 'fr@nssddev'
     }
-});
-async function run(){
-    const mailSent = await transporter.sendMail({
-        text: 'Texto do email',
-        subject: 'Assunto do email',
-        from: 'francivaldo costa<frncvldreisdev@gmail.com',
-        to: ['frncvldreisdev@gmail.com','frncvldreisdev@gmail.com'],
-    });
-    console.log(mailSent);
+  });
+var mailOptions = {
+    from: 'frncvldreisdev@gmail.com',
+    to: 'frncvldreisdev@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+function test(){
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+        console.log(error);
+        } else {
+        console.log('Email sent: ' + info.response);
+        }
+    }); 
 }
-run();
+export default test;
