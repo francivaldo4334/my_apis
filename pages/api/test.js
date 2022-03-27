a28d3e324f2ad106842add416931e95a7ab87980
francivaldo4334 <frncvldreis@gmail.com>
2022-03-27 00:05:44 -0300

    email test 1

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,3 +1,17 @@
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'frncvldreis@gmail.com',
+      pass: 'Fr@nssdd4334'
+    }
+  });
+var mailOptions = {
+    from: 'frncvldreis@gmail.com',
+    to: 'frncvldreis@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
 function test(request,response){
     response.json({
         name:"fran"
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+// https://my-apis.vercel.app/
 export default function Home(){
     return <div>My Apis</div>
 }
